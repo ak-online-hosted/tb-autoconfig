9c71e58eabdb5b0867913d69880752e9c7a460a0
root <kandre@ak-online.be>
2012-07-05 21:40:33 +0200

    add documentation and first template

--- index.php
+++ index.php
@@ -6,6 +6,16 @@
 
 	// config section
 
+	/*
+		a few assumptions are made here:
+			* a domain has always the same server for IMAP and SMTP (this might need to be changed some day)
+			* if smtp_port and imap_port are not defined use 993 for IMAP and 25 for SMTP
+			* if smtp_ssl and imap_ssl are not defined use SSL for IMAP and TLS for SMTP
+			* if username_format is undefined use only localpart
+			* if
+	*/
+
+	$domains["ak-online.be"]["server"] = "debs.ak-online.be";
 
 	// do matching and output correct config-file
 
