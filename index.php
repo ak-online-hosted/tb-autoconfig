b5ec6c2924ef3ba58ef80eb28b772427badf56e1
root <kandre@ak-online.be>
2012-07-05 21:10:14 +0200

    add documentation, remove debug

--- index.php
+++ index.php
@@ -1,11 +1,17 @@
 <?php
 
+	// always required doctype
 	$DOCTYPE = '<?xml version="1.0" encoding="UTF-8"?>';
-
 	$result = $DOCTYPE."\n";
+
+	// config section
+
+
+	// do matching and output correct config-file
 
 	if ( preg_match ( "/autoconfig.(.*)$/" , $_SERVER["SERVER_NAME"] , $matches ) > 0 ) {
 		$domain = $matches[1];
+
 
 		echo $result;
 
