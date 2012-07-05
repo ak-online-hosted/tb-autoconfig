5943068855b87cd4020abde3ccfdae77015bd33f
root <kandre@ak-online.be>
2012-07-05 21:07:14 +0200

    implement basic extraction of $domain from the vhost-name that was hit

--- index.php
+++ index.php
@@ -4,8 +4,13 @@
 
 	$result = $DOCTYPE."\n";
 
-	
+	if ( preg_match ( "/autoconfig.(.*)$/" , $_SERVER["SERVER_NAME"] , $matches ) > 0 ) {
+		$domain = $matches[1];
 
+		echo $result;
 
-	echo $result;
+	} else {
+		header("HTTP/1.0 404 Not Found", 404 );
+		exit;
+	}
 ?>
