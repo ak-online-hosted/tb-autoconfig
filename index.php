f4eb12a0a12681258183518ece28f5c99d0ffb34
Andre Klärner <kandre@ak-online.be>
2012-07-06 01:00:08 +0200

    implement the real magic for consolidating the settings

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+	// vim: tabstop=3 :
 
 	// always required doctype
 	$DOCTYPE = '<?xml version="1.0" encoding="UTF-8"?>';
@@ -11,7 +12,7 @@
 			* a domain has always the same server for IMAP and SMTP (this might need to be changed some day)
 			* if smtp_port and imap_port are not defined use 993 for IMAP and 25 for SMTP
 			* if smtp_ssl and imap_ssl are not defined use SSL for IMAP and TLS for SMTP
-			* if username_format is undefined use only localpart
+			* if username_format is undefined use only localpart (%EMAILLOCALPART%)
 	*/
 
 	$domains["ak-online.be"]["server"] = "debs.ak-online.be";
@@ -21,33 +22,43 @@
 	if ( preg_match ( "/autoconfig.(.*)$/" , $_SERVER["SERVER_NAME"] , $matches ) > 0 ) {
 		$domain = $matches[1];
 
-		$result .= "<clientConfig version=\"1.1\">\n";
-		$result .= "  <emailProvider id=\"ak-online.be\">\n";
-		$result .= "    <domain>ak-online.be</domain>\n";
-		$result .= "    <displayName>AK-ONLINE Mail</displayName>\n";
-		$result .= "    <displayShortName>ak-online.be</displayShortName>\n";
-		$result .= "\n";
-		$result .= "    <incomingServer type=\"imap\">\n";
-		$result .= "      <hostname>debs.ak-online.be</hostname>\n";
-		$result .= "      <port>993</port>\n";
-		$result .= "      <socketType>SSL</socketType>\n";
-		$result .= "      <authentication>password-cleartext</authentication>\n";
-		$result .= "      <username>%EMAILLOCALPART%</username>\n";
-		$result .= "    </incomingServer>\n";
-		$result .= "\n";
-		$result .= "    <outgoingServer type=\"smtp\">\n";
-		$result .= "      <hostname>debs.ak-online.be</hostname>\n";
-		$result .= "      <port>465</port>\n";
-		$result .= "      <socketType>SSL</socketType>\n";
-		$result .= "      <authentication>password-cleartext</authentication>\n";
-		$result .= "      <username>%EMAILLOCALPART%</username>\n";
-		$result .= "    </outgoingServer>\n";
-		$result .= "\n";
-		$result .= "  </emailProvider>\n";
-		$result .= "</clientConfig>\n";
+		if ( is_array( $domains[$domain] ) ) {
+			$server = isset( $domains[$domain]["server"] ) ? $domains[$domain]["server"] : NULL;
+			if ( $server === NULL ) exit;
 
-		echo $result;
+			$imap_port = isset( $domains[$domain]["imap_port"] ) ? $domains[$domain]["imap_port"] : "993";
+			$imap_ssl = isset( $domains[$domain]["imap_ssl"] ) ? $domains[$domain]["imap_ssl"] : "SSL";
 
+			$smtp_port = isset( $domains[$domain]["smtp_port"] ) ? $domains[$domain]["smtp_port"] : "25";
+			$smtp_ssl = isset( $domains[$domain]["smtp_ssl"] ) ? $domains[$domain]["smtp_ssl"] : "STARTTLS";
+
+			$username_format = isset( $domains[$domain]["username_format"] ) ? $domains[$domain]["username_format"] : "%EMAILLOCALPART%";
+
+			$result .= "<clientConfig version=\"1.1\">\n";
+			$result .= "  <emailProvider id=\"".$domain."\">\n";
+			$result .= "    <domain>".$domain."</domain>\n";
+			$result .= "\n";
+			$result .= "    <incomingServer type=\"imap\">\n";
+			$result .= "      <hostname>".$server."</hostname>\n";
+			$result .= "      <port>".$imap_port."</port>\n";
+			$result .= "      <socketType>".$imap_ssl."</socketType>\n";
+			$result .= "      <authentication>password-cleartext</authentication>\n";
+			$result .= "      <username>".$username_format."</username>\n";
+			$result .= "    </incomingServer>\n";
+			$result .= "\n";
+			$result .= "    <outgoingServer type=\"smtp\">\n";
+			$result .= "      <hostname>".$server."</hostname>\n";
+			$result .= "      <port><".$smtp_port."/port>\n";
+			$result .= "      <socketType>".$smtp_ssl."</socketType>\n";
+			$result .= "      <authentication>password-cleartext</authentication>\n";
+			$result .= "      <username>".$username_format."</username>\n";
+			$result .= "    </outgoingServer>\n";
+			$result .= "\n";
+			$result .= "  </emailProvider>\n";
+			$result .= "</clientConfig>\n";
+
+			echo $result;
+		}
 	} else {
 		header("HTTP/1.0 404 Not Found", 404 );
 		exit;
